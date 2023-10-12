f6cf26c5683a9d539827a2d92c572e4d9e92d4b9
Mercen <mercen@mercen.net>
2023-10-12 09:01:57 +0900

    UPDATE: Package Information

--- Package.swift
+++ Package.swift
@@ -5,19 +5,17 @@ import PackageDescription
 
 let package = Package(
     name: "AppGPTLang",
+    platforms: [.iOS(.v14)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "AppGPTLang",
             targets: ["AppGPTLang"]),
     ],
+    dependencies: [
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "AppGPTLang"),
-        .testTarget(
-            name: "AppGPTLangTests",
-            dependencies: ["AppGPTLang"]),
+            name: "AppGPTLang",
+            dependencies: [])
     ]
 )
